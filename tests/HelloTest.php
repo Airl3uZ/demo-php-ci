ac49133eb2bd7f4bd260fb04c9ac1077166aebe3
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-03-25 19:05:22 +0700

    update docker-compose

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -2,10 +2,10 @@
 class HelloTest extends PHPUnit_Framework_TestCase {
     
     public function testXXX() {
-        $expectedResult = "Hello, somkiat";
+        $expectedResult = "Hello, Airl3uZ";
         $hello = new Hello();
 
-        $actualResult = $hello->sayHi("somkiat");
+        $actualResult = $hello->sayHi("Airl3uZ");
 
         $this->assertEquals($expectedResult, $actualResult);
     }
